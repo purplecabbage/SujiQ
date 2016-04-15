8194afbdd759c6d81149ee8ce62f18c6e9ad0f5b
Jesse MacFadyen <purplecabbage@gmail.com>
2016-04-14 23:36:24 -0700

    fix layout and background parallax to be dynamic

--- app/www/app.js
+++ app/www/app.js
@@ -22,8 +22,10 @@ var currentView = "easyDiv";
 
 function init() {
 
+    // window.navigator.standalone
+
 	getNumber();
-	
+
 	mQ(".numBtn").forEach(function(item){
 		item.addEventListener("mousedown",onTableCellClick);
 	});
@@ -31,11 +33,11 @@ function init() {
 	mQ("#info")[0].addEventListener("mousedown",getNumber);
 	mQ(".delBtn")[0].addEventListener("mousedown",onDelClick);
 	mQ(".solveBtn").forEach(function (item) {
-		item.addEventListener("mousedown",getAnswer); 
+		item.addEventListener("mousedown",getAnswer);
 	});
-	
+
 	loadNextBG();
-	
+
 	doSelected(0);
 }
 
@@ -80,10 +82,10 @@ function onDelClick(e) {
 
 
 function doSelected(arg) {
-	
+
     var left="0px";
 	var bgLeft = "0px";
-	
+
 
 	var deltaWidth = ( window.innerWidth - 640 ) / 2;
 	var diff = window.innerWidth / 3;
@@ -91,39 +93,39 @@ function doSelected(arg) {
 	var tabBGPos = (deltaWidth - diff) + "px";
 
 	//window.alert('deltaWidth ' + deltaWidth);
-
+    var bgOffset = Math.floor(window.innerWidth / 4);
 
     switch(arg) {
-        case 1 : { //"entryDiv" : 
+        case 1 : { //"entryDiv" :
             left = "-100%";
-			bgLeft = "-25%";
+			bgLeft = -bgOffset + "px";
 			tabBGPos = deltaWidth + "px";
             break;
         }
-        case 2 : { // "aboutDiv" :  
+        case 2 : { // "aboutDiv" :
             left = "-200%";
-			bgLeft = "-50%";
+			bgLeft = -(2 * bgOffset) + "px";
 			tabBGPos = (deltaWidth + diff) + "px";
             break;
         }
     }
-    
+
     mQ("#easyDiv")[0].style.transform = "translate3D(" + left + ",0px,0px)";
     mQ("#entryDiv")[0].style.transform = "translate3D(" + left + ",0px,0px)";
-    mQ("#aboutDiv")[0].style.transform = "translate3D(" + left + ",0px,0px)";    
-	mQ("#bodyBG")[0].style.transform = "translate3D(" + bgLeft + ",0px,0px)";   
+    mQ("#aboutDiv")[0].style.transform = "translate3D(" + left + ",0px,0px)";
+	mQ("#bodyBG")[0].style.transform = "translate3D(" + bgLeft + ",0px,0px)";
 	mQ("#tabSelector")[0].style.backgroundPosition = tabBGPos + " 0";
 	//mQ("#tabSelector")[0].style.transform = "translate3D(" + tabBGPos + ",0px,0px)";
 
 	currentView = arg;
-	
+
 }
 
 function getAnswer() {
 	var answer = "";
 	var ones = currentNum % 10;
 	var tens = ((currentNum % 100 ) - ones) / 10;
-	
+
 	if(currentNum == 0) { // special case
 		answer += numbers[ones];
 	}
@@ -131,12 +133,12 @@ function getAnswer() {
 		if(tens > 0) {
 			answer += tens == 1 ? "jyu " : numbers[tens] + " jyu ";
 		}
-	
+
 		if(ones > 0) {
 			answer += numbers[ones];
 		}
 	}
-	
+
 	mQ(".answerDiv").forEach(function(elem) {
 		elem.innerText = answer;
 	});
